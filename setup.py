86647c4a3218c8de839f2a4b32a81610e844539b
nick_reith <nick_reith@dell.com>
2023-05-04 15:12:32 -0500

    bump to version 0.2.0 for new feature

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="jupyter-tensorboard-proxy",
-    version='0.1.1',
+    version='0.2.0',
     url="https://github.com/kopwei/jupyter-tensorboard-proxy",
     author="kopkop",
     description="kopkop@gmail.com",
