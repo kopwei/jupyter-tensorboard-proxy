c1636513a85a98f2cedd88f04d5d2ae62cfef97e
nick_reith <nick_reith@dell.com>
2023-05-04 15:54:51 -0500

    Add options for user to specify alternate TF_LOG_DIR, either as an environment variable, or in a ~/.tensorboard file. This enables alternate paths and also allows using s3 or s3-compatible storage as the tensorboard --log-dir location, especially in environments (jhub), where users may not have full control of this launcher.

--- jupyter_tensorboard_proxy/__init__.py
+++ jupyter_tensorboard_proxy/__init__.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import logging
+from dotenv import load_dotenv
 
 
 logger = logging.getLogger(__name__)
@@ -21,15 +22,33 @@ def setup_tensorboard():
         executable = shutil.which('tensorboard')
         if not executable:
             raise FileNotFoundError('Can not find tensorboard executable in $PATH')
-        # Create theia working directory
-        home_dir = os.environ.get('HOME') or "/home/%s" % os.environ.get('NB_USER') or '/home/jovyan'
-        working_dir = f'{home_dir}'
-        if not os.path.exists(working_dir):
-            os.makedirs(working_dir)
-            logger.info("Created directory %s" % working_dir)
-        else:    
-            logger.info("Directory %s already exists" % working_dir)
-        return ['tensorboard', '--logdir', '%s/logs' % home_dir, '--port', '{port}']
+
+        # NOTE: With the edits below, it will expect to find the TF_LOG_DIR variable
+        #   either already exported in the current shell session, or located in a ~/.tensorboard file
+        # In addition, if using s3 for the location, it will also expect to find these env vars in the same location
+        #   AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION, S3_ENDPOINT_URL (if not official AWS)
+
+        # first look for --logdir override in the form of an s3 bucket or alternate path, then fall back to home_dir if not found
+        if os.environ.get('TF_LOG_DIR'):
+            TF_LOG_DIR = os.environ.get('TF_LOG_DIR')
+        # providing a second option to store this in a dot file, as env vars are context/session specific
+        elif os.path.isfile("%s/.tensorboard" % os.environ.get('HOME')):
+            load_dotenv("%s/.tensorboard" % os.environ.get('HOME'))
+            if not os.environ.get('TF_LOG_DIR'):
+                logger.error("If you wish to change the tensorflow/tensorboard TF_LOG_DIR via config file instead of env var, you must put 'TF_LOG_DIR=path' in the $HOME/.tensorboard file")
+            else:
+                TF_LOG_DIR = os.environ.get('TF_LOG_DIR')
+        else:
+            # Create working directory
+            home_dir = os.environ.get('HOME') or "/home/%s" % os.environ.get('NB_USER') or '/home/jovyan'
+            working_dir = f'{home_dir}'
+            if not os.path.exists(working_dir):
+                os.makedirs(working_dir)
+                logger.info("Created directory %s" % working_dir)
+            else:
+                logger.info("Directory %s already exists" % working_dir)
+            TF_LOG_DIR = '%s/logs' % home_dir
+        return ['tensorboard', '--logdir', TF_LOG_DIR, '--port', '{port}']
     
     return {
         'command': _get_tensorboard_command,
